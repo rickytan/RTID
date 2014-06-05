cb73de9e7d6924f0a1bd18155d855716da917da1
rickytan <ricky.tan.xin@gmail.com>
2014-06-05 16:27:08 +0800

    Initial Commit

---
+++ RTID/RTID.h
@@ -0,0 +1,13 @@
+//
+//  RTID.h
+//  RTID
+//
+//  Created by ricky on 14-6-5.
+//  Copyright (c) 2014年 ricky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RTID : NSObject
+
+@end
---
+++ RTID/RTID.m
@@ -0,0 +1,13 @@
+//
+//  RTID.m
+//  RTID
+//
+//  Created by ricky on 14-6-5.
+//  Copyright (c) 2014年 ricky. All rights reserved.
+//
+
+#import "RTID.h"
+
+@implementation RTID
+
+@end
